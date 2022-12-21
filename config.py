6ea496a1557af170e3f6ac6d1fcac9bf1d57e94d
palagina-kristina <palagina-kristina1@mail.ru>
2022-12-22 01:56:37 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5864940564:AAFw3WCwRcdDgNLTs1KaJAZBH8vEVOxBbJ8"
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USD",
+    "евро": "EUR",
+    "иена": "JPY",
+    "рубль": "RUB",
+    "юань": "CNY"
+}
